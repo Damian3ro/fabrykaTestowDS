693498c2e2b4bc60a1f6f1efea15bffeeedad74f
Damian Stasik <stasik.damian7@gmail.com>
2023-12-28 20:42:44 +0100

    Homework for module 6 finished

---
+++ modul_6/test_mod6_praca_domowa.py
@@ -0,0 +1,21 @@
+import unittest
+from selenium import webdriver
+from pages import iframe_page
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get('https://simpletestsite.fabrykatestow.pl/')
+        self.driver.maximize_window()
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test1_iframe_click_first_button(self):
+        iframe_page.click_iframe_tab(self.driver)
+        if iframe_page.iframe_content_visible(self.driver):
+            iframe_page.show_whole_iframe_element(self.driver)
+            iframe_page.click_on_button_inside_iframe(self.driver, 1)
+
+        self.driver.save_screenshot('iframe_found_screenshot.png')
